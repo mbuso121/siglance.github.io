b5ce13883cbe9c7ae509b7669b3dd040a7e2529e
mbuso <mbusongobese43@gmail.com>
2025-05-30 01:07:23 +0200

    Initial commit of my portfolio

---
+++ script.js
@@ -0,0 +1,10 @@
+// Fade in social icons when they scroll into view
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('visible');
+    }
+  });
+});
+
+document.querySelectorAll('.social-icons').forEach(el => observer.observe(el));
